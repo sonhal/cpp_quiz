c55cfd8fafae8bedd6983ac539c80f52afc9a333
=Sondre <=sondre.hal@gmail.com>
2017-05-03 23:49:17 +0200

    made start function and logic, added logic to main function

--- main.cpp
+++ main.cpp
@@ -1,23 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/* 
- * File:   main.cpp
- * Author: sondr
- *
- * Created on 27. april 2017, 21:21
- */
 
 #include <cstdlib>
 
 using namespace std;
 
 
-int main(int argc, char** argv) {
+void start(int *q, string *username){
+    
+    
+    bool haschosen;
+    cout << "Wecome to the Quiz App " << endl  << " Please type your username " << endl;
+    cin >> getline(username, 100);
+    cout << "Hello " + username + " What Quiz would you like to do?" << endl;
+ 
+    
+    while(haschosen)
+    cout << "Type 1 for: History" << endl << "Type 2 for: Computer Science" << endl << "Type 3 for: Nature" << endl;
+    cin >> q;
+    
+    switch(q){
+        
+        case q == 1 :
+            haschosen = false;
+            break;
+        
+        case q == 2 :
+            haschosen = false;
+            break;    
+        
+        case q == 3 :
+            haschosen = false;
+            break;  
+            
+        default  :
+           cout << "Type 1,2 or 3" << endl;
+            break;
+           
+    }
+    
+   
+    
+    
+    
+    
+}
 
+
+
+
+
+
+int main(int argc, char** argv) {
+      string username;
+      int q;
+      
+      void start(*q,*username);
+      
+      Quiz quiz(q,username);
+      
+      
+    
+    
+    
     return 0;
 }
 
